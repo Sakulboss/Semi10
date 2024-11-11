5254792d904e72142f83fd1c10933cbf14735781
Sakulboss <sdose9899@gmail.com>
2024-11-11 17:50:54 +0100

    Update record.py

--- record.py
+++ record.py
@@ -13,9 +13,9 @@ print('Programmene')
 write('output.wav', fs, myrecording)  # Save as WAV file
 write('output1.npy', fs, myrecording)  # Save as npy??? file
 print('Programmene')	
-with open("output2.npy", "w+") as file:
-	file.write(str(myrecording))
-with open("output3.wav", "w+") as file:
-	file.write(str(myrecording))
+#with open("output2.npy", "w+") as file:
+#	file.write(str(myrecording))
+#with open("output3.wav", "w+") as file:
+#	file.write(str(myrecording))
 	
 print('Jetzt reichst mir jetzt rede ich! ES IST OBST IM HAUS ')	
