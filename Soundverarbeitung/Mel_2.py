9de835a80d30c67b2dad2a6cdf4d30e258ceb565
Konradcmn <123997118+Darnok007@users.noreply.github.com>
2024-12-05 17:20:53 +0100

    Update Mel_2.py

--- Soundverarbeitung/Mel_2.py
+++ Soundverarbeitung/Mel_2.py
@@ -52,9 +52,9 @@ plt.show()"""
 # hop_length = Number of samples between successive STFT frames (default: n_fft // 2) (wie viele abschnitte wollen wir haben?
 # n_mels = Anzahl der Mel-Banden die generiert werden (standart 128)
 # fmin & fmax = minimum und maximum der frequenzen in den Mel-Koeffizienten (standard durch librosa.filters.mel())
-
+fmax = 1500
 #Mel Spectrogramm wird extrahiert
-mel_spectrogram = librosa.feature.melspectrogram(y = y, sr = sr, fmin = 0, fmax= 2000, hop_length = 100)
+mel_spectrogram = librosa.feature.melspectrogram(y = y, sr = sr, fmax = fmax, hop_length= )
 
 # ref = Weißt daraufhin Amplitude zu nutzen, wenn Umwandlung in Decibel
 #Lineares Spectrogramm wird zu Decibel (logarithmus) umgewandelt
@@ -62,7 +62,7 @@ mel_spectrogram_db = librosa.power_to_db(mel_spectrogram, ref = np.max)
 #cmap = colormap die genutzt wird (cmap='viridis')
 #Mel Spectrogramm plotten
 plt.figure(figsize=(8, 8))
-librosa.display.specshow(mel_spectrogram_db, x_axis = None, y_axis = 'mel', sr = sr) #'time'
+librosa.display.specshow(mel_spectrogram_db, x_axis = 'time', y_axis = 'mel', sr = sr, fmax = fmax) #'time'
 plt.colorbar(format='%+2.0f dB')
 plt.title('Mel Spectrogram')
 plt.show()
