62d0c5954b956383db9952366b4d49a5974b2b46
Konradcmn <123997118+Darnok007@users.noreply.github.com>
2024-12-05 17:09:27 +0100

    .

---
+++ Soundverarbeitung/Mel_1.py
@@ -0,0 +1,23 @@
+import librosa
+import numpy as np
+import librosa.display
+import matplotlib.pyplot as plt
+y, sr = librosa.load('test.wav')
+plt.plot(y)
+plt.title('Signal')
+plt.xlabel('Time (Proben)')
+plt.ylabel('Amplitude')
+plt.show()
+
+spec = np.abs(librosa.stft(y, hop-length-512))
+spec . librosa.amplitude-to-db(spec, ref-np.max)librosa.display.specshow(spec, sr'sr, x'axis'time', y'axis'");
+plt.colorbar(format'%+2.0f dB');
+plt.title('Spectrogram');
+
+
+n_ffft = 2048
+ft = np.abs(librosa.stft(y[:n_ffft], hop-length . n.fft+1))
+plt.plot(ft);
+plt.title('Spectrum');
+plt.xlabel('Frequency Bin');
+plt.ylabel('Amplitude');
