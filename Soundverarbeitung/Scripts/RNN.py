c0046caf405e6bae12fa137d9a19aa376e8d84c6
Konradcmn <123997118+Darnok007@users.noreply.github.com>
2024-12-17 13:54:17 +0100

    .

--- Soundverarbeitung/Scripts/RNN.py
+++ Soundverarbeitung/Scripts/RNN.py
@@ -1,9 +1,12 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import SimpleRNN, Dense
+#from tensorflow.keras.models import Sequential
+#from tensorflow.keras.layers import SimpleRNN, Dense
+from keras.models import Sequential
+from keras.layers import SimpleRNN, Dense
 
-text = "This is GeeksforGeeks a software training institute"
+
+text = "1, 2, 3, 4, 5, 6, 7, 8, 9,False, 10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25, True"
 chars = sorted(list(set(text)))
 char_to_index = {char: i for i, char in enumerate(chars)}
 index_to_char = {i: char for i, char in enumerate(chars)}
@@ -25,16 +28,31 @@ X_one_hot = tf.one_hot(X, len(chars))
 y_one_hot = tf.one_hot(y, len(chars))
 
 model = Sequential()
-model.add(SimpleRNN(50, input_shape=(seq_length, len(chars)), activation='relu'))
+model.add(SimpleRNN(100, input_shape=(seq_length, len(chars)), activation='relu'))
 model.add(Dense(len(chars), activation='softmax'))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(X_one_hot, y_one_hot, epochs=100)
+model.fit(X_one_hot, y_one_hot, epochs=500)
 
-start_seq = "This is G"
+start_seq = "10,11,"
 generated_text = start_seq
 
-for i in range(50):
+for i in range(45):
+    x = np.array([[char_to_index[char] for char in generated_text[-seq_length:]]])
+    x_one_hot = tf.one_hot(x, len(chars))
+    prediction = model.predict(x_one_hot)
+    next_index = np.argmax(prediction)
+    next_char = index_to_char[next_index]
+    generated_text += next_char
+
+print("Generated Text:")
+print(generated_text)
+print('\nThe length of the generated text is: ', len(generated_text))
+
+start_seq = "1, 2, "
+generated_text = start_seq
+
+for i in range(45):
     x = np.array([[char_to_index[char] for char in generated_text[-seq_length:]]])
     x_one_hot = tf.one_hot(x, len(chars))
     prediction = model.predict(x_one_hot)
