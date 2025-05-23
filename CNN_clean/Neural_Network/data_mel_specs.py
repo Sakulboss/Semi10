ddf39eb85eef8e2f696330cc7a5268f92916ee79
Sakulboss <150686334+Sakulboss@users.noreply.github.com>
2025-05-23 19:56:01 +0200

    Hotfix

--- CNN_clean/Neural_Network/data_mel_specs.py
+++ CNN_clean/Neural_Network/data_mel_specs.py
@@ -84,7 +84,7 @@ def mel_specs(labels, setting, logger):
     # Create mel spectrograms
     #if logger.getEffectiveLevel() == logging.INFO:
     for count in tqdm(range(len(fn_wav_list)), desc='Mel-Cepstogramm'):
-        mel_spec = mel_spec_file(fn_wav_list[count], stereo=(size == 'bees_1'))
+        mel_spec = mel_spec_file(fn_wav_list[count], logger, stereo=(size == 'bees_1'))
         if mel_spec is None or count != 0 and mel_spec.shape != all_mel_specs[-1].shape:
             error_files.append(fn_wav_list[count])
         else:
